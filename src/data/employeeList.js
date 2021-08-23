99013fb30d4b9e363c0d45d3cd3044692ecf26ef
awesomeusha <78401938+awesomeusha@users.noreply.github.com>
2021-08-23 13:20:56 +0530

    static employee list js  added

---
+++ src/data/employeeList.js
@@ -0,0 +1,62 @@
+export const employeeList = [
+  {
+    firsName: "Usha",
+    lastName: "Wadhwani",
+    email: "usha@gmail.com",
+    primaryLanguage: "English",
+  },
+  {
+    firsName: "Monica",
+    lastName: "Wadhwani",
+    email: "monica@gmail.com",
+    primaryLanguage: "English",
+  },
+  {
+    firsName: "Mayur",
+    lastName: "Wadhwani",
+    email: "mayur@gmail.com",
+    primaryLanguage: "English",
+  },
+  {
+    firsName: "Teena",
+    lastName: "Wadhwani",
+    email: "teena@gmail.com",
+    primaryLanguage: "English",
+  },
+  {
+    firsName: "Reema",
+    lastName: "Wadhwani",
+    email: "reema@gmail.com",
+    primaryLanguage: "English",
+  },
+  {
+    firsName: "Rashmi",
+    lastName: "Wadhwani",
+    email: "rashmi@gmail.com",
+    primaryLanguage: "English",
+  },
+  {
+    firsName: "Chirag",
+    lastName: "Wadhwani",
+    email: "chirag@gmail.com",
+    primaryLanguage: "English",
+  },
+  {
+    firsName: "Sumit",
+    lastName: "Wadhwani",
+    email: "sumit@gmail.com",
+    primaryLanguage: "English",
+  },
+  {
+    firsName: "Payal",
+    lastName: "Wadhwani",
+    email: "payal@gmail.com",
+    primaryLanguage: "English",
+  },
+  {
+    firsName: "Usha",
+    lastName: "Wadhwani",
+    email: "usha@gmail.com",
+    primaryLanguage: "English",
+  },
+];
